33e046a2d89b867e10cc673c098d7ffd96be1702
sai1741 <113848917+sai1741@users.noreply.github.com>
2024-01-02 15:14:10 +0530

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		
+		//println : gives nest line after end of statement
+		
+		System.out.println("hello world");
+		System.out.print("hello world");
+	}
+
+}
---
+++ sumOfTwo.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class sumOfTwo {
+
+	public static void main(String[] args) {
+		int a=25;
+		int b=30;
+		System.out.print(a+b);
+
+	}
+
+}
